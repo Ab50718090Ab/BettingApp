bc948ec65170b541394423daf17e59c274c7278b
Ab50718090Ab <mithuray8708@gmail.com>
2026-04-23 21:50:55 +0600

    Update page.tsx

---
+++ apps/web/app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const RegisterPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-xl font-bold">Register Page</h1>
+    </div>
+  );
+};
+
+export default RegisterPage;
