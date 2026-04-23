dc81948b3b1d9ddd5df834bf6e905ad50f334676
Ab50718090Ab <mithuray8708@gmail.com>
2026-04-23 20:01:42 +0600

    Delete apps/backend directory

--- apps/backend/validators/bets.js
+++
@@ -1,9 +0,0 @@
-import { body } from 'express-validator';
-
-const validatePlaceBet = [
-  body('gameId').isString().notEmpty(),
-  body('amount').isFloat({ min: 0.01 }),
-  body('parameters').optional().isObject(),
-];
-
-export default { validatePlaceBet };
